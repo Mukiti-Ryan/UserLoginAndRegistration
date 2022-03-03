76acced3e0b50b39bfea321db2f274fcfca26e0a
Mukiti-Ryan <87067381+Mukiti-Ryan@users.noreply.github.com>
2022-03-03 12:13:57 +0300

    Initial commit

---
+++ src/main/java/com/example/Authentication/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.Authentication.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/example/Authentication/registration/RegistrationService.java
@@ -0,0 +1,31 @@
+package com.example.Authentication.registration;
+
+import com.example.Authentication.appuser.AppUser;
+import com.example.Authentication.appuser.AppUserRole;
+import com.example.Authentication.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
+    public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email is not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
+    }
+}
