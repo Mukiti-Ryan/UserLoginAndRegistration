490d437c2311ffd9f349facd369bceffebb13106
Mukiti-Ryan <87067381+Mukiti-Ryan@users.noreply.github.com>
2022-03-03 20:37:35 +0300

    Email Package done

--- src/main/java/com/example/Authentication/appuser/AppUserService.java
+++ src/main/java/com/example/Authentication/appuser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.example.Authentication.appuser;
 
+import com.example.Authentication.registration.token.ConfirmationToken;
+import com.example.Authentication.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -14,6 +19,7 @@ public class AppUserService implements UserDetailsService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final static String USER_NOT_FOUND_MSG = "The user with the email %s is not found";
     private final AppUserRepository appUserRepository;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -31,7 +37,23 @@ public class AppUserService implements UserDetailsService {
 
         appUser.setPassword(encodedPassword);
         appUserRepository.save(appUser);
-//        TODO: Send confirmation token
-        return "It works";
+
+        String  token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+//        TODO: SEND EMAIL
+
+        return token;
+    }
+
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
     }
 }
---
+++ src/main/java/com/example/Authentication/email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.example.Authentication.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
